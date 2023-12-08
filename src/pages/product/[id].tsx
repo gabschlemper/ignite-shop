555337a029659a05c63328ff8a8e06834ed40aec
Gabriela Schlemper <gabschlemper@gmail.com>
2023-12-08 16:41:22 -0300

    Add style to product page

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,6 +1,28 @@
 import { useRouter } from "next/router";
+import {
+  ImageContainer,
+  ProductContainer,
+  ProductDetails,
+} from "../../styles/pages/product";
 
 export default function Product() {
   const { query } = useRouter();
-  return <h1>product {JSON.stringify(query)}</h1>;
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>€ 79.99</span>
+        <p>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
+          quibusdam perspiciatis doloribus facilis distinctio porro tenetur
+          optio ea! Ad rerum magnam modi quos eaque exercitationem? Officia
+          totam repellat doloremque architecto.
+        </p>
+
+        <button>Buy</button>
+      </ProductDetails>
+    </ProductContainer>
+  );
 }
